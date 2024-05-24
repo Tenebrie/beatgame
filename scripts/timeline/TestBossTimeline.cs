118e3fc598ed869ba822a73a340b960513680b3b
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-05-24 20:37:33 +0300

    feat: rework training room environment

--- scripts/timeline/TestBossTimeline.cs
+++ scripts/timeline/TestBossTimeline.cs
@@ -4,6 +4,7 @@ public partial class TestBossTimeline : BaseTimeline<TestBoss>
 {
 	public TestBossTimeline(TestBoss parent) : base(parent)
 	{
+		return;
 		for (var i = 1; i < 20; i++)
 			Add(i * 4, parent.AutoAttack);
 	}
